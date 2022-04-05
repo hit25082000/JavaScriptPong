a07e4b8ac0bd6f8ee8133c8b577caf53c4cb942e
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2022-04-05 17:09:42 -0400

    Raquete oponente

--- JavaScriptPong.js
+++ JavaScriptPong.js
@@ -1,34 +1,56 @@
+//Variaveis Bolinha
 let xBol = 300;
 let yBol = 200;
 let dBol = 15;
 let raioBol = dBol / 2;
 
-let VelXBol = 2;
-let VelYBol = 2;
+let VelXBol = 6;
+let VelYBol = 6;
 
+//Variaveis minha raquete
 let xRaq = 5
 let yRaq = 150
+
+//Variaveis raeuete oponente
+let xRaqOpo = 585;
+let yRaqOpo = 150;
+let VelYOpo;
+
+//Medida padrão de raquetes
 let cRaq = 10
 let aRaq = 90
+
+//Placar
+let MeusPontos = 0;
+let PontosOponente = 0;
 
 
 function setup() {
   createCanvas(600, 400);
 }
 
+let coli50false
+
 function draw() {
   background(0);
   MostrarBolinha();
   MovimentarBolinha();
   VerificarColisaoBorda(); 
-  MostrarRaquete();
+  MostrarRaquete(xRaq,yRaq);
   MovimentarRaquete();
-  VerificarColisaoRaquete();
+  //VerificarColisaoRaquete();
+  colisaoMinhaRaqueteBiblioteca(xRaq,yRaq);
+  colisaoMinhaRaqueteBiblioteca(xRaqOpo,yRaqOpo);
+  MostrarRaquete(xRaqOpo,yRaqOpo);
+  MovimentarRaqueteOponente();
+  incluirPlacar();
+  MarcaPonto();
 }
 
-function MostrarRaquete(){
-    rect(xRaq,yRaq,cRaq,aRaq)
+function MostrarRaquete(x,y){
+    rect(x,y,cRaq,aRaq)
 }
+
 function MostrarBolinha(){
     circle(xBol,yBol,dBol)
 }
@@ -60,5 +82,32 @@ function VerificarColisaoBorda(){
 function VerificarColisaoRaquete(){
   if(xBol - raioBol < xRaq + cRaq && yBol- raioBol < yRaq + aRaq && yBol + raioBol + yRaq){
     VelXBol *= -1;
+  }
+}
+
+function colisaoMinhaRaqueteBiblioteca(x,y) {
+  colidiu = collideRectCircle(x, y, cRaq, aRaq, xBol, yBol, raioBol);
+  if(colidiu){
+    VelXBol *= -1;
+  }
+}
+
+function MovimentarRaqueteOponente(){
+  VelYOpo = yBol - yRaqOpo - cRaq/2 - 30;
+  yRaqOpo += VelYOpo
+}
+
+function incluirPlacar(){
+  fill(255);
+  text(MeusPontos,278,26);
+  text(PontosOponente,231,26);
+}
+
+function MarcaPonto(){
+  if(xBol>590){
+    MeusPontos += 1;
+  }
+  if(xBol<10){
+    PontosOponente += 1;
   }
 }
